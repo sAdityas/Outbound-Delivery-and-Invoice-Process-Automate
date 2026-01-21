42fe3518ce57f22f690f33c01ed5f1ac02b0bf1a
sAdityas <adissss009@gmail.com>
2026-01-21 11:03:06 +0530

    .

--- backend/TransactionSeventh/asnDetails.py
+++ backend/TransactionSeventh/asnDetails.py
@@ -1,3 +1,5 @@
+import time
+
 def asnDetails(session,invoice_array,plant,BillType):
     BillType = list(BillType)
     session.findById("wnd[0]/usr/ctxtS_FKART-LOW").text = BillType[0] 
@@ -8,5 +10,5 @@ def asnDetails(session,invoice_array,plant,BillType):
     session.findById("wnd[0]/usr/ctxtS_VBELN-LOW").text = invoice_array[0]
     session.findById("wnd[0]/usr/ctxtS_VBELN-HIGH").text =invoice_array[-1]
     session.findById("wnd[0]").sendVKey(8)
-    
+    time.sleep(1)
     session.findById("wnd[0]/tbar[0]/btn[3]").press()
